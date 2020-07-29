1252046b8e61b40fe759d3495218b5c23547ec7b
ShunyaSuzuki <shunya_suzuki@ShunyaSuzukinoMacBook-Air.local>
2020-07-29 11:51:41 +0900

    update

--- front-page.php
+++ front-page.php
@@ -2,43 +2,67 @@
 $home = esc_url(home_url());
 $wp_url = get_template_directory_uri();
 get_header(); ?>
-<section class="py-5">
-  <div class="container">
-    <div class="mb-5">
-      <h2>テキストカラー一覧</h2>
-      <p class="text-body">text-body</p>
-      <p class="text-primary">text-primary</p>
-      <p class="text-secondary">text-secondary</p>
-      <p class="text-info">text-info</p>
-      <p class="text-success">text-success</p>
-      <p class="text-warning">text-warning</p>
-      <p class="text-danger">text-danger</p>
-      <p class="text-light">text-light</p>
-      <p class="text-dark">text-dark</p>
-    </div>
-    <div class="mb-5">
-      <h2>ボタン一覧</h2>
-      <div class="mb-3">
-        <button class="mr-2 btn btn-primary">text-primary</button>
-        <button class="mr-2 btn btn-secondary">text-secondary</button>
-        <button class="mr-2 btn btn-info">text-info</button>
-        <button class="mr-2 btn btn-success">text-success</button>
-        <button class="mr-2 btn btn-warning">text-warning</button>
-        <button class="mr-2 btn btn-danger">text-danger</button>
-        <button class="mr-2 btn btn-light">text-light</button>
-        <button class="mr-2 btn btn-dark">text-dark</button>
-      </div>
-      <div class="mb-0">
-        <button class="mr-2 btn btn-outline-primary">text-primary</button>
-        <button class="mr-2 btn btn-outline-secondary">text-secondary</button>
-        <button class="mr-2 btn btn-outline-info">text-info</button>
-        <button class="mr-2 btn btn-outline-success">text-success</button>
-        <button class="mr-2 btn btn-outline-warning">text-warning</button>
-        <button class="mr-2 btn btn-outline-danger">text-danger</button>
-        <button class="mr-2 btn btn-outline-light">text-light</button>
-        <button class="mr-2 btn btn-outline-dark">text-dark</button>
-      </div>
-    </div>
-  </div>
+
+<div class="mv">
+<div id="mv-carousel" class="carousel slide carousel-fade" data-ride="carousel">
+<div class="carousel-inner">
+<div class="carousel-item active" data-interval="5000">
+<img src="<?php echo $wp_url; ?>/dist/images/mv_pc_01.png" class="d-block w-100" alt="スライドショー" srcset="<?php echo $wp_url; ?>/dist/images/mv_pc_01.png 1x, <?php echo $wp_url; ?>/dist/images/mv_pc_01@2x.png 2x">
+</div>
+<div class="carousel-item" data-interval="5000">
+<img src="<?php echo $wp_url; ?>/dist/images/mv_pc_02.png" class="d-block w-100" alt="スライドショー" srcset="<?php echo $wp_url; ?>/dist/images/mv_pc_02.png 1x, <?php echo $wp_url; ?>/dist/images/mv_pc_02@2x.png 2x">
+</div>
+<div class="carousel-item" data-interval="5000">
+<img src="<?php echo $wp_url; ?>/dist/images/mv_pc_03.png" class="d-block w-100" alt="スライドショー" srcset="<?php echo $wp_url; ?>/dist/images/mv_pc_03.png 1x, <?php echo $wp_url; ?>/dist/images/mv_pc_03@2x.png 2x">
+</div>
+</div>
+</div>
+<!-- #mv-carousel -->
+
+<div class="mv-verticaltxt">
+<img src="<?php echo $wp_url; ?>/dist/images/mv_vertical.png" alt="おしゃれな人のための着物レンタル" srcset="<?php echo $wp_url; ?>/dist/images/mv_vertical.png 1x, <?php echo $wp_url; ?>/dist/images/mv_vertical@2x.png 2x">
+</div>
+
+<div class="mv-jaran">
+<img src="<?php echo $wp_url; ?>/dist/images/jaran_pc.png" alt="じゃらん" srcset="<?php echo $wp_url; ?>/dist/images/jaran_pc.png 1x, <?php echo $wp_url; ?>/dist/images/jaran_pc@2x.png 2x">
+<img src="<?php echo $wp_url; ?>/dist/images/member_pc.png" alt="じゃらん" srcset="<?php echo $wp_url; ?>/dist/images/member_pc.png 1x, <?php echo $wp_url; ?>/dist/images/member_pc@2x.png 2x">
+</div>
+
+<div class="mv-txtkimono">
+<img src="<?php echo $wp_url; ?>/dist/images/txt_kimono.png" alt="kimono" srcset="<?php echo $wp_url; ?>/dist/images/txt_kimono.png 1x, <?php echo $wp_url; ?>/dist/images/txt_kimono@2x.png 2x">
+</div>
+
+</div>
+
+<div class="home-about py-5 text-center">
+<div class="container">
+<img src="<?php echo $wp_url; ?>/dist/images/top_about_pc.png" alt="kimono" srcset="<?php echo $wp_url; ?>/dist/images/top_about_pc.png 1x, <?php echo $wp_url; ?>/dist/images/top_about_pc@2x.png 2x">
+</div>
+</div>
+
+<div class="py-5">
+<div class="infinity-slide">
+<div class="infinity-slide-inner">
+<?php for ($i=1; $i <= 4; $i++): ?>
+<div class="infinity-slide-inner-slide">
+<img src="<?php echo $wp_url; ?>/dist/images/about_img_0<?php echo $i; ?>.png" alt="kimono" srcset="<?php echo $wp_url; ?>/dist/images/about_img_0<?php echo $i; ?>.png 1x, <?php echo $wp_url; ?>/dist/images/about_img_0<?php echo $i; ?>@2x.png 2x">
+</div>
+<?php endfor; ?>
+<?php for ($i=1; $i <= 4; $i++): ?>
+<div class="infinity-slide-inner-slide">
+<img src="<?php echo $wp_url; ?>/dist/images/about_img_0<?php echo $i; ?>.png" alt="kimono" srcset="<?php echo $wp_url; ?>/dist/images/about_img_0<?php echo $i; ?>.png 1x, <?php echo $wp_url; ?>/dist/images/about_img_0<?php echo $i; ?>@2x.png 2x">
+</div>
+<?php endfor; ?>
+</div>
+</div>
+</div>
+
+<section class="home-plan py-5">
+<img class="bg-icon" src="<?php echo $wp_url; ?>/dist/images/bg_icon.png" alt="kimono" srcset="<?php echo $wp_url; ?>/dist/images/bg_icon.png 1x, <?php echo $wp_url; ?>/dist/images/bg_icon@2x.png 2x">
+<div class="container">
+<h2 class="ttl-2 text-center"><span>PLAN</span>プラン紹介</h2>
+<p class="text-center">着物コーディネートを楽しめるプランからお任せプランまで、<br>様々な種類のプランの中からあなたにぴったりの着物レンタルプランをお選びいただけます。</p>
+</div>
 </section>
+
 <?php get_footer();
--- header.php
+++ header.php
@@ -2,22 +2,50 @@
 $home = esc_url(home_url());
 $wp_url = get_template_directory_uri(); ?>
 <!DOCTYPE HTML>
-<html lang="ja">
+<html <?php language_attributes(); ?>>
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width,initial-scale=1.0">
-
 <?php wp_head(); ?>
-
 <?php if (!is_user_logged_in()): ?>
-<!-- トラッキングコード -->
 <?php endif; ?>
 </head>
 
-<body>
+<body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 
-<header>
+<header class="header">
+<h1>京都観光にぴったりな<br>着物・浴衣レンタル店 京あるき</h1>
+<div class="header-logo">
+<a href="<?php echo $home; ?>">
+<img src="<?php echo $wp_url; ?>/dist/images/logo.png" alt="<?php bloginfo('name'); ?>" srcset="<?php echo $wp_url; ?>/dist/images/logo.png 1x, <?php echo $wp_url; ?>/dist/images/logo@2x.png 2x">
+</a>
+</div>
+
+<nav class="header__nav">
+<div class="header__nav-language">
+<select class="form-control">
+<option>Languages</option>
+<option>日本語</option>
+<option>English</option>
+<option>簡体中文</option>
+<option>繁體中文</option>
+</select>
+</div>
+<ul class="header__nav-list">
+<li class="header__nav-item"><a class="header__nav-link" href="<?php echo $home; ?>/point/"><span class="playfair text-danger text-uppercase">point</span>おすすめポイント</a></li>
+<li class="header__nav-item"><a class="header__nav-link" href="<?php echo $home; ?>/plan/"><span class="playfair text-danger text-uppercase">plan</span>プラン一覧</a></li>
+<li class="header__nav-item"><a class="header__nav-link" href="<?php echo $home; ?>/option/"><span class="playfair text-danger text-uppercase">option</span>オプションサービス</a></li>
+<li class="header__nav-item"><a class="header__nav-link" href="<?php echo $home; ?>/flow/"><span class="playfair text-danger text-uppercase">flow</span>着物レンタルの流れ</a></li>
+<li class="header__nav-item"><a class="header__nav-link" href="<?php echo $home; ?>/shop/"><span class="playfair text-danger text-uppercase">shop</span>店舗一覧</a></li>
+<li class="header__nav-item"><a class="header__nav-link" href="<?php echo $home; ?>/faq/"><span class="playfair text-danger text-uppercase">faq</span>よくあるご質問</a></li>
+<li class="header__nav-item"><a class="header__nav-link" href="<?php echo $home; ?>/group/"><span class="playfair text-danger text-uppercase">group</span>団体のお客様へ</a></li>
+</ul>
+<div class="header__nav-btn mt-3">
+<a class="btn btn-block btn-danger" href="<?php echo $home; ?>">WEB予約</a>
+</div>
+</nav>
+
 </header>
 
 <main>
