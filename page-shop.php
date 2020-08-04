e99a6d70605527fd1772a5fc5d9b125ffb28aa76
ShunyaSuzuki <shunya_suzuki@ShunyaSuzukinoMacBook-Air.local>
2020-08-04 15:44:17 +0900

    update

---
+++ page-shop.php
@@ -0,0 +1,217 @@
+<?php
+$home = esc_url(home_url());
+$wp_url = get_template_directory_uri();
+get_header(); the_post(); ?>
+
+<section class="mv__sub">
+<img class="bg-icon" src="<?php echo $wp_url; ?>/dist/images/bg_icon.png" alt="kimono" srcset="<?php echo $wp_url; ?>/dist/images/bg_icon.png 1x, <?php echo $wp_url; ?>/dist/images/bg_icon@2x.png 2x">
+<div class="container">
+<h2 class="text-danger text-center">
+<span class="font-serif d-block">SHOP</span>
+<span class="font-serif d-block">店舗一覧</span>
+</h2>
+</div>
+<img class="bg-icon" src="<?php echo $wp_url; ?>/dist/images/bg_icon.png" alt="kimono" srcset="<?php echo $wp_url; ?>/dist/images/bg_icon.png 1x, <?php echo $wp_url; ?>/dist/images/bg_icon@2x.png 2x">
+</section>
+
+<div class="pb-5 shop">
+<div class="container">
+
+<div class="shop__inner">
+<h3 class="font-serif">京都四条本店</h3>
+<div class="embed-responsive embed-responsive-4by3 gmap">
+<iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13072.392008767352!2d135.7581414!3d35.0042518!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xdf3ab54ea777acf5!2z5Lqs44GC44KL44GNIOS6rOmDveWbm-adoeacrOW6lw!5e0!3m2!1sja!2sjp!4v1596517738623!5m2!1sja!2sjp"></iframe>
+</div>
+<p class="shop__inner-address">〒604-8154<br>京都市中京区室町通四条上ル菊水鉾町582 さがの館ビル2F</p>
+<p class="shop__inner-info">TEL：075-213-7171<br>営業時間 10:00～19:00<br>最終受付 17:00</p>
+<p>※ 定休日は年末年始です。</p>
+<p class="shop__inner-access p-3 bg-light rounded">【阪急電車「烏丸駅」】 24番出口より徒歩2分
+<br>【京都市営地下鉄「四条駅」】 24番出口より徒歩2分
+<br>【京都駅】 より地下鉄と徒歩で約6分</p>
+<div class="shop__inner-point">
+<h4><span class="font-serif text-danger text-uppercase">Point!</span></h4>
+<h5 class="font-serif text-center">最大300名様迄対応可能の<br>『京都最大級店舗』</h5>
+<p>京都中心部（四条駅/烏丸駅から徒歩2分・京都駅より６分）に本店があり、『錦市場』をぶらぶら歩き、『祇園』『八坂神社』『清水寺』『八坂庚申堂』『高台寺』等の『祇園東山エリア』『伏見稲荷』や『嵐山』…どこへでもアクセス抜群！ホテル・旅館の多いエリアなのでスケジュールが組みやすいですよ♪</p>
+</div>
+<div class="shop__inner-seeing">
+<p class="text-center">
+<img src="<?php echo $wp_url; ?>/dist/images/shop_pict_1.png" alt="京都四条本店" srcset="<?php echo $wp_url; ?>/dist/images/shop_pict_1.png 1x, <?php echo $wp_url; ?>/dist/images/shop_pict_1@2x.png 2x">
+</p>
+<div class="shop__inner-seeing-reco bg-light">
+<h4 class="font-serif">おすすめ観光地</h4>
+<div class="shop__inner-seeing-reco__list">
+<div class="shop__inner-seeing-reco__list-item">
+<h5>嵐山</h5>
+<p>電車で<span class="font-serif text-danger">20</span>分</p>
+</div>
+<div class="shop__inner-seeing-reco__list-item">
+<h5>清水寺周辺</h5>
+<p>バスで<span class="font-serif text-danger">15</span>分</p>
+</div>
+<div class="shop__inner-seeing-reco__list-item">
+<h5>伏見稲荷</h5>
+<p>電車で<span class="font-serif text-danger">25</span>分</p>
+</div>
+<div class="shop__inner-seeing-reco__list-item">
+<h5>平安神宮</h5>
+<p>バスで<span class="font-serif text-danger">20</span>分</p>
+</div>
+<div class="shop__inner-seeing-reco__list-item">
+<h5>金閣寺</h5>
+<p>バスで<span class="font-serif text-danger">30</span>分</p>
+</div>
+<div class="shop__inner-seeing-reco__list-item">
+<h5>錦市場</h5>
+<p>徒歩で<span class="font-serif text-danger">5</span>分</p>
+</div>
+<div class="shop__inner-seeing-reco__list-item">
+<h5>六角堂</h5>
+<p>徒歩で<span class="font-serif text-danger">5</span>分</p>
+</div>
+<div class="shop__inner-seeing-reco__list-item">
+<h5>元離宮二条城</h5>
+<p>電車で<span class="font-serif text-danger">10</span>分</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+<!-- .shop__inner -->
+
+<div class="shop__inner">
+<h3 class="font-serif">京都祇園店</h3>
+<div class="embed-responsive embed-responsive-4by3 gmap">
+<iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3268.290125526158!2d135.77534271518437!3d34.99944187466738!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x600108c5b032c3d5%3A0xf0461d4533246bb!2z5Lqs44GC44KL44GNIOa4heawtOmrmOWPsOWvuuW6lw!5e0!3m2!1sja!2sjp!4v1596520057038!5m2!1sja!2sjp"></iframe>
+</div>
+<p class="shop__inner-address">〒605-0823<br>京都府京都市東山区下弁天町492F/3F(2F受付)<br>ファミリーマートと安井金比羅宮のある交差点の目の前(鳥居のある交差点)が当店です。<br>１階にカフェがある４階建ての建物です。</p>
+<p class="shop__inner-info">TEL：075-541-7761<br>営業時間 10:00～19:00<br>最終受付 17:00</p>
+<p>※ 定休日は年末年始です。</p>
+<p class="shop__inner-access p-3 bg-light rounded">【JR京都駅より】バスと徒歩で約20分
+<br>【京阪「祇園四条駅」】バスと徒歩で8分、徒歩で15分
+<br>【阪急「河原町駅」】バスと徒歩で10分、徒歩で17分</p>
+<div class="shop__inner-point">
+<h4><span class="font-serif text-danger text-uppercase">Point!</span></h4>
+<h5 class="font-serif text-center">最大60名様迄対応可能の<br>『祇園・東山エリア最大級店舗』</h5>
+<p>清水寺・八坂神社・八坂庚申堂・祇園・高台寺などの祇園・東山エリアの観光地の中心地です。<br>高台寺や八坂庚申堂までは徒歩3分♪</p>
+</div>
+<div class="shop__inner-seeing">
+<p class="text-center">
+<img src="<?php echo $wp_url; ?>/dist/images/shop_pict_2.png" alt="京都祇園店" srcset="<?php echo $wp_url; ?>/dist/images/shop_pict_2.png 1x, <?php echo $wp_url; ?>/dist/images/shop_pict_2@2x.png 2x">
+</p>
+<div class="shop__inner-seeing-reco bg-light">
+<h4 class="font-serif">おすすめ観光地</h4>
+<div class="shop__inner-seeing-reco__list">
+<div class="shop__inner-seeing-reco__list-item">
+<h5>高台寺</h5>
+<p>徒歩で<span class="font-serif text-danger">3</span>分</p>
+</div>
+<div class="shop__inner-seeing-reco__list-item">
+<h5>安井金比羅宮</h5>
+<p>徒歩で<span class="font-serif text-danger">1</span>分</p>
+</div>
+<div class="shop__inner-seeing-reco__list-item">
+<h5>清水寺周辺</h5>
+<p>徒歩で<span class="font-serif text-danger">10</span>分</p>
+</div>
+<div class="shop__inner-seeing-reco__list-item">
+<h5>八坂庚申堂</h5>
+<p>徒歩で<span class="font-serif text-danger">2</span>分</p>
+</div>
+<div class="shop__inner-seeing-reco__list-item">
+<h5>祇園・花見小路</h5>
+<p>徒歩で<span class="font-serif text-danger">5</span>分</p>
+</div>
+<div class="shop__inner-seeing-reco__list-item">
+<h5>八坂神社</h5>
+<p>徒歩で<span class="font-serif text-danger">7</span>分</p>
+</div>
+<div class="shop__inner-seeing-reco__list-item">
+<h5>建仁寺</h5>
+<p>徒歩で<span class="font-serif text-danger">3</span>分</p>
+</div>
+<div class="shop__inner-seeing-reco__list-item">
+<h5>知恩院</h5>
+<p>徒歩で<span class="font-serif text-danger">15</span>分</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+<!-- .shop__inner -->
+
+<div class="shop__inner">
+<h3 class="font-serif">大阪梅田店</h3>
+<div class="embed-responsive embed-responsive-4by3 gmap">
+<iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3279.9931073423036!2d135.49468751517762!3d34.70535379047684!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000e68e1e373ad3%3A0xdac555451549855!2z44CSNTMwLTAwMTIg5aSn6Ziq5bqc5aSn6Ziq5biC5YyX5Yy66Iqd55Sw77yS5LiB55uu77yR4oiS77yR!5e0!3m2!1sja!2sjp!4v1596520457884!5m2!1sja!2sjp"></iframe>
+</div>
+<p class="shop__inner-address">〒530-0012<br>大阪市北区芝田2丁目1番1号<br>阪急芝田町ビル3F<br>(京都さがの館 大阪梅田店にて受付)
+<br>ヨドバシカメラ梅田店・リンクス梅田の北東側の交差点の目の前です。<br>1F・2Fがウェンディーズファーストキッチンのビルです。</p>
+<p class="shop__inner-info">TEL：06-6374-5589<br>営業時間 11:00～19:00<br>最終受付 17:00</p>
+<p>※ 定休日は年末年始です。</p>
+<p class="shop__inner-access p-3 bg-light rounded">【阪急「梅田駅」】御堂筋北口から徒歩2分<br>【JR「大阪駅」】御堂筋北口から徒歩2分<br>【地下鉄御堂筋線「梅田駅」】御堂筋北口から徒歩3分</p>
+<div class="shop__inner-point">
+<h4><span class="font-serif text-danger text-uppercase">Point!</span></h4>
+<p class="mb-0">大阪駅や梅田駅からすぐ！アクセス抜群で立ち寄りやすいお店です。</p>
+</div>
+<div class="shop__inner-seeing">
+<p class="text-center">
+<img src="<?php echo $wp_url; ?>/dist/images/shop_pict_3.png" alt="大阪梅田店" srcset="<?php echo $wp_url; ?>/dist/images/shop_pict_3.png 1x, <?php echo $wp_url; ?>/dist/images/shop_pict_3@2x.png 2x">
+</p>
+<div class="shop__inner-seeing-reco bg-light">
+<h4 class="font-serif">おすすめ観光地</h4>
+<div class="shop__inner-seeing-reco__list">
+<div class="shop__inner-seeing-reco__list-item">
+<h5>大阪城</h5>
+<p>電車で<span class="font-serif text-danger">15</span>分</p>
+</div>
+<div class="shop__inner-seeing-reco__list-item">
+<h5>露天神社(お初天神)</h5>
+<p>徒歩で<span class="font-serif text-danger">15</span>分</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+<!-- .shop__inner -->
+
+<div class="shop__inner">
+<h3 class="font-serif">大阪なんば店</h3>
+<div class="embed-responsive embed-responsive-4by3 gmap">
+<iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3281.613711238397!2d135.49698601517676!3d34.664457292666086!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000e76d21c11c31%3A0x9d7f8863c5516c79!2z5Y2X5rW36YeO5p2R44OT44Or!5e0!3m2!1sja!2sjp!4v1596521373076!5m2!1sja!2sjp"></iframe>
+</div>
+<p class="shop__inner-address">〒556-0011<br>大阪市浪速区難波中1-10-4南海野村ビル2F<br>(京都さがの館 なんば店にて受付)<br>1FがCAFE VELOCEのビルです。</p>
+<p class="shop__inner-info">TEL：06-6633-2150<br>営業時間 11:00～19:00<br>最終受付 17:00</p>
+<p>※ 定休日は年末年始です。</p>
+<p class="shop__inner-access p-3 bg-light rounded">地下鉄・南海・近鉄・JR「難波」駅より地下5番出口から西へ徒歩1分</p>
+<div class="shop__inner-point">
+<h4><span class="font-serif text-danger text-uppercase">Point!</span></h4>
+<p class="mb-0">なんばの繁華街のすぐ近く！心斎橋や道頓堀を楽しみたい人にもおすすめのお店です。</p>
+</div>
+<div class="shop__inner-seeing">
+<p class="text-center">
+<img src="<?php echo $wp_url; ?>/dist/images/shop_pict_4.png" alt="大阪なんば店" srcset="<?php echo $wp_url; ?>/dist/images/shop_pict_4.png 1x, <?php echo $wp_url; ?>/dist/images/shop_pict_4@2x.png 2x">
+</p>
+<div class="shop__inner-seeing-reco bg-light">
+<h4 class="font-serif">おすすめ観光地</h4>
+<div class="shop__inner-seeing-reco__list">
+<div class="shop__inner-seeing-reco__list-item">
+<h5>法善寺横丁</h5>
+<p>徒歩で<span class="font-serif text-danger">10</span>分</p>
+</div>
+<div class="shop__inner-seeing-reco__list-item">
+<h5>なんばグランド花月</h5>
+<p>徒歩で<span class="font-serif text-danger">7</span>分</p>
+</div>
+<div class="shop__inner-seeing-reco__list-item">
+<h5>道頓堀</h5>
+<p>徒歩で<span class="font-serif text-danger">10</span>分</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+<!-- .shop__inner -->
+
+</div>
+</section>
+<?php get_footer();
